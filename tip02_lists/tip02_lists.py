830e627578b9512832ba62562276cec3fb0cb4b3
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-02-29 06:36:08 -0300

    Python Tips 02 - Lists

---
+++ tip02_lists/tip02_lists.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Lists
+#
+########################################
+#
+#
+#
+list_02 = ['Charlie Chaplin', 'Vincent van Gogh', 'Wolfgang A. Mozart', 'Ludwig van Beethoven']
+
+print(len(list_02)) # To know the lenght of the list / Para saber o tamanho da lista
+
+print(list_02[0]) # To print the element on the first position (starts with 0) / Para exibir o elemento na primeira posição (começa com 0)
+
+for i in list_02: # To iterates over a list and print all items / Para exibir todos os itens de uma lista
+    print(i)
+
+for index, names in enumerate(list_02): # To index a list and iterates over all items / Para indexar uma lista e exibir todos os itens
+    print(index, names)
