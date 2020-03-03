de4049768e4ceab7cf87e54ee819994c7415b4ad
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-03 07:13:21 -0300

    Python Tips 05 - Boolean

---
+++ tip05_boolean/tip05_boolean.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Numbers
+# 2020-03-03
+#
+########################################
+#
+#
+
+# Boolean type returns True or False as a result
+# Tipo boleano retorna True, se o valor corresponde ou False se não corresponde como resultado
+verdade = True
+falso = False
+
+soma = 6 + 3
+
+resultado = input("Digite o resultado da soma de (6 + 3): ")
+
+if resultado == "9":
+    print(verdade)
+else:
+    print(falso)
