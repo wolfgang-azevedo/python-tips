3b4d91d1c21e0db658c657a8dca6bd7593b932b4
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-13 08:09:00 -0300

    Python Tips 15 - Enumerate

---
+++ tip15_enumerate/tip15_enumerate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Enumerate Built-in Function
+# 2020-03-13
+#
+########################################
+#
+#
+routers = ['ROUTER01', 'ROUTER02', 'ROUTER03', 'ROUTER04', 'ROUTER02']
+
+# With Enumerate built-in function you can enumerate an object
+# Com a função interna Enumerate, você pode enumerar objetos
+print(list(enumerate(routers)))
