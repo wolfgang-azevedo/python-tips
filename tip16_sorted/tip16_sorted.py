74ad9a956997b552d5fabd682824cc0a78ecccdc
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-14 07:30:27 -0300

    Python Tips 16 - Sorted

---
+++ tip16_sorted/tip16_sorted.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Sorted Built-in Function
+# 2020-03-14
+#
+########################################
+#
+#
+routers = ['ROUTER01', 'ROUTER03', 'ROUTER05', 'ROUTER02', 'ROUTER04']
+
+# With Sorted built-in function you can sort an object list
+# Com a função interna Sorted, você pode ordenar uma lista de objetos
+print(sorted(routers))
