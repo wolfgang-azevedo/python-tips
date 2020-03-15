3c8ad930b27b595a29abfef98c35b67dfef4f0ee
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-15 11:41:45 -0300

    Python Tips 17 - Dir

---
+++ tip17_dir/tip17_dir.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Dir Built-in Function
+# 2020-03-15
+#
+########################################
+#
+#
+import requests
+
+# With Dir built-in function you can list all availble arguments for an object
+# Com a função interna Dir, você lista todos os argumentos disponíveis de um objeto
+print(dir(requests))
