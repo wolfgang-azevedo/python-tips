dafc3c4c790a9a617e1613de8e1ab7cfc302c25c
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-19 05:48:19 -0300

    Python Tips 20 - Modules

---
+++ tip20_modules/tip20_modules.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Importing Modules
+# 2020-03-19
+#
+########################################
+#
+#
+# To import a module, you must call the import following by the name of module
+# Para importar um módulo, você deverá chamar o import seguido pelo nome do módulo
+import calendar
+
+
+ano = 20
+mes = 3
+
+print(calendar.month(ano, mes))
