6abae1d767a3b61a48569793567628403b6159c8
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-23 09:10:26 -0300

    Python Tips 23 - Conversion

---
+++ tip23_conversion/tip23_conversion.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Built-in method Oct, Bin and Hex
+# 2020-03-23
+#
+########################################
+#
+#
+
+### Built-in methods Oct, Bin and Hex, you can convert a integer number to Octal, Binary and Hexadecimal
+### Os métodos embarcados Oct and Bin, você converter um número inteiro em Octal, Binário e Hexadecimal
+
+int_to_oct = 12
+int_to_bin = 24
+int_to_hex = 30
+
+#To convert an Integer to Octal
+print(oct(int_to_oct))
+
+#To convert an Integer to Binary
+print(bin(int_to_bin))
+
+#To convert an Integer to Hexadecimal
+print(hex(int_to_hex))
