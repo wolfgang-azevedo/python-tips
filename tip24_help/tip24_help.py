25ebf9b4132f10b6236fee66057d200f62be02c4
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-25 10:20:30 -0300

    Python Tips 24 - Help

---
+++ tip24_help/tip24_help.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Built-in method Help
+# 2020-03-24
+#
+########################################
+#
+#
+
+### Built-in method help, invoke the help system for a given argument
+### O método embarcado Help, envoca o sistema de ajuda para o argumento informado
+
+help(print)
