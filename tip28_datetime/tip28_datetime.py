6c3fd11882626464d1e944183ba2c04e5a41d75c
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-28 18:26:55 -0300

    Python Tips 28 - Datetime

---
+++ tip28_datetime/tip28_datetime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Module Datetime
+# 2020-03-28
+#
+########################################
+#
+#
+from datetime import datetime
+
+timestamp = datetime.now()
+
+print(timestamp)
