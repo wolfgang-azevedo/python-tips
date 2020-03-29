4e23ea3544b731d88693318fef63e5e0272e504b
wolfazevedo@gmail.com <wolfazevedo@gmail.com>
2020-03-29 08:12:36 -0300

    Python Tips 29 - Getpass

---
+++ tip29_getpass/tip29_getpass.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.8
+#
+########################################
+# 
+# Python Tips, by Wolfgang Azevedo
+# https://github.com/wolfgang-azevedo/python-tips
+#
+# Module Getpass
+# 2020-03-29
+#
+########################################
+#
+#
+import getpass
+
+password = getpass.getpass()
+
+print('Você digitou: ', password)
